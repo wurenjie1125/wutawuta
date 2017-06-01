5454019b2a25b2f0860cf9c07b46c84206bfb75e
wurenjie <464995040@qq.com>
2017-06-01 18:59:16 +0800

    init

---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <!--<img src="./assets/logo.png">-->
+    <router-view></router-view>
+  </div>
+</template>
+<style lang="sass">
+  @import './styles/main.scss';
+</style>
+
+<script>
+//  require('./styles/main.scss')
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/components/listen.vue
@@ -0,0 +1,58 @@
+<template>
+  <section class="page listen">
+    <div class="player-btn">
+      <span @click="togglePlay">play</span>
+    </div>
+    <audio class="player" src="../music/a.mp3">
+      <p>Your browser does not support the <code>audio</code> element.</p>
+    </audio>
+    <div class="cd" data-rotation="360" :class="{rotate:isRotate}"></div>
+    <div class="lrc">
+
+    </div>
+  </section>
+</template>
+<style>
+  .cd{
+    background:url('../images/eroman.jpg') top center;
+  }
+</style>
+<script>
+  var TWEEN = require('tween');
+  export default {
+    data(){
+          return {
+            musicSrc:'music/a.mp3',
+            cd:{},
+            audio:{},
+            isRotate:false,
+          }
+      },
+    mounted(){
+      this.audio = document.querySelector('.player');
+      this.cd = document.querySelector('.cd');
+      new TWEEN.Tween(this.audio.dataset).to({rotation:360},10000).start();
+      animate();
+      function animate(){
+        window.requestAnimationFrame(animate);
+        // [...]
+        TWEEN.update();
+        // [...]
+      }
+
+    },
+    methods:{
+
+        togglePlay(){
+            this.isRotate = !this.isRotate;
+            if(this.isRotate){
+              this.audio.play();
+            }else{
+              this.audio.pause();
+
+            }
+        }
+    }
+
+  }
+</script>
